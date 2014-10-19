3ca56e4d43bf972e4398c58cd43c592c70e20a42
Anthony Garo <agaro1121@gmail.com>
2014-10-19 15:07:59 -0400

    Adding uTorrent Post Script

---
+++ uTorrentPostScript.ps1
@@ -0,0 +1,51 @@
+﻿param (
+    [string]$u = "",
+    [string]$p = ""
+
+ )
+ #Write-Output "user = $($u)"
+ #Write-Output "pass = $($p)"
+
+$UTORRENT_URL = "http://localhost:63251/gui/"
+$REGEX_UTORRENT_TOKEN = 
+@"
+<div[^>]*id=[\"\']token[\"\'][^>]*>([^<]*)</div>
+"@
+
+$webclient = new-object System.Net.WebClient
+$credCache = new-object System.Net.CredentialCache
+$creds = new-object System.Net.NetworkCredential($u,$p)
+$credCache.Add($UTORRENT_URL, "Basic", $creds)
+$webclient.Credentials = $credCache
+$webpage = $webclient.DownloadString($UTORRENT_URL+ "token.html")
+$cookies =  $webClient.ResponseHeaders["Set-Cookie"]
+$webclient.Headers.Add("Cookie",$cookies)
+
+$webpage -match $REGEX_UTORRENT_TOKEN > $null
+$token = $matches[1]
+
+#$UTORRENT_TOKENIZED_URL = $UTORRENT_URL + "?list=1&token=" + $token
+$UTORRENT_TOKENIZED_URL = $UTORRENT_URL + "?token=$($token)"
+
+$response = $webClient.DownloadString($UTORRENT_URL + "?list=1&token=" + $token)
+$json = ConvertFrom-JSON $response
+
+#$dict=@{}
+    
+$json.torrents | Foreach-Object {
+    #$dict.add($_[2],$_)
+    if($_[21].startswith("Finished") -or $_[21].startswith("Seeding") -or $_[21].startswith("Queued seed")){
+            #Write-Output "Removing the Following Torrent: "
+            #Write-Output "Name: $($_[2])"
+            #Write-Output "Hash: $($_[0])"
+            #Write-Output "Status: $($_[21])"
+            #Write-Output "Path: $($_[26])"
+            $current_url = $UTORRENT_TOKENIZED_URL+"&action=remove&hash=$($_[0])"
+            $response = $webClient.DownloadString($current_url)
+            [string]$fullPath = $_[26]
+            [string]$name = $_[2]
+            $path = $fullPath.Substring(0,$fullPath.Length-$name.Length)
+            $path
+            rm -Path ($path+"*") -Include *.torrent
+    }
+} 
